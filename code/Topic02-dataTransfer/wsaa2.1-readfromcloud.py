7bb420e43193d172e355c811c4cf243538f8251b
Andrew Beatty <andrew.beatty@atu.ie>
2026-02-04 19:21:57 +0000

    topic 02 sample code now works

--- code/Topic02-dataTransfer/wsaa2.1-readfromcloud.py
+++ code/Topic02-dataTransfer/wsaa2.1-readfromcloud.py
@@ -1,14 +1,18 @@
+# newer version of a program that gets the bit coin price in euro
+# This was tested on the 4 Feb 2026 and works
+# Author: Andrew Beatty
+
 import requests
 import json
 # this is flakey at the moment
-url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/btc.json"
 response = requests.get(url)
 data = response.json()
 #with open("bitcoindump.json", "w") as fp:
 #    json.dump(data, fp)
 
-euroobject = data["bpi"]["EUR"]
-rate = euroobject["rate"]
-print(rate)
+eurorate = data["btc"]["eur"]
+
+print(f"bitcoin in euroa is €{eurorate:.2f}")
 
 
